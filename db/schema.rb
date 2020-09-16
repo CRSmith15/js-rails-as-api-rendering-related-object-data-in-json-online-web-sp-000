bb178082b3a4785e1ff63f1fe0382a1b186b444a
Chance Smith <chancesmith97@hotmail.com>
2020-09-16 23:32:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_233034) do
+ActiveRecord::Schema.define(version: 2019_05_10_115410) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
     t.string "species"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
